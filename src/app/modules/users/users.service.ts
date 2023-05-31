15b40ebbdb6936af92206eb1a5be007baa03db6e
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-31 17:33:17 +0600

    create default password

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,9 +1,14 @@
+import config from '../../../config'
 import { IUser } from './users.interface'
 import { User } from './users.model'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated incremental id
+
   // default password
+  if (!user.password) {
+    user.password = config.default_user_pass as string
+  }
 
   const createdUser = await User.create(user)
 
